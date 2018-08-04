770fb0362ee17add44be4b7ac2c57c3c91cce268
Melissa Wong <mellymai@gmail.com>
2018-08-03 18:44:45 -0700

    Fixes Filter component's render

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Filter = <div>This is a placeholder for Filters Area</div>;
+const Filter = () => <div>This is a placeholder for Filters Area</div>;
 
 export default Filter;
