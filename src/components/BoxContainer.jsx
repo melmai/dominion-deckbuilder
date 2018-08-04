145d8399f1d967445dda1bb4887e1c1ef1048be2
Melissa Wong <mellymai@gmail.com>
2018-08-04 12:07:27 -0700

    Removes extra container in BoxContainer

--- src/components/BoxContainer.jsx
+++ src/components/BoxContainer.jsx
@@ -5,14 +5,12 @@ import Box from './Box';
 class BoxContainer extends Component {    
     render() {
         return (
-            <div className="box-container">
-                <div className="boxes">
-                    <Box name="Dominion2" toggleBox={this.props.toggleBox} />
-                    <Box name="Intrigue2" toggleBox={this.props.toggleBox} />
-                    <Box name="Adventures" toggleBox={this.props.toggleBox} />
-                    <Box name="Nocturne" toggleBox={this.props.toggleBox} />
-                </div>
-            </div>  
+            <section className="box__container">
+                <Box name="Dominion2" toggleBox={this.props.toggleBox} />
+                <Box name="Intrigue2" toggleBox={this.props.toggleBox} />
+                <Box name="Adventures" toggleBox={this.props.toggleBox} />
+                <Box name="Nocturne" toggleBox={this.props.toggleBox} />
+            </section>  
         );
     }
 }
