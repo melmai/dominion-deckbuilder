432f91a6131d0378aa5c68677b194ccbb3cea7fd
Melissa Wong <mellymai@gmail.com>
2018-08-08 23:06:29 -0700

    Adds colored banners for cards

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -20,15 +20,29 @@ export const Card = (props) => {
         }
     }
 
+    let bannerClass = (type, start) => {
+        let name = start;
+        if (type.includes('Duration') && type.includes('Reaction')) name = `${name} card__banner_duration-reaction`;
+        if (type.includes('Treasure') && type.includes('Reserve')) name = `${name} card__banner_treasure-reserve`;
+        if (type.includes('Treasure') && type.includes('Victory')) name = `${name} card__banner_treasure-victory`;
+        if (type.includes('Reserve') && type.includes('Victory')) name = `${name} card__banner_reserve-victory`;
+        if (type.includes('Duration')) name = `${name} card__banner_duration`;
+        if (type.includes('Reserve')) name = `${name} card__banner_reserve`;
+        if (type.includes('Victory')) name = `${name} card__banner_victory`;
+        if (type[0] === 'Treasure') name = `${name} card__banner_treasure`;
+        if (type[0] === 'Reaction') name = `${name} card__banner_reaction`;
+        return name;
+    }
+
     return (
         <section className="card">
-            <h1 className="card__name">{props.card.name}</h1>
+            <h1 className={bannerClass(props.card.class, 'card__name')}>{props.card.name}</h1>
             <div className="card__icon_container">{icon(props.card.box)}</div>
             <div className="card__divider" />
             <div className="card__text_container">
                 <p className="card__text">{props.card.text}</p>
             </div>
-            <div className="card__bottom_banner">
+            <div className={bannerClass(props.card.class, 'card__bottom_banner')}>
                 <p className="card__type">{cardType}</p>
                 <span className="card__cost">{props.card.cost}</span>
             </div>
