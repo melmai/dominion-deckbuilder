ef6f6461b3c1879421aed09b97d45b06a4a106f8
Melissa Wong <mellymai@gmail.com>
2018-08-10 13:21:05 -0700

    Adds card abilities to text container

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -1,24 +1,44 @@
 import React from 'react';
 
+const AbilityList = (props) => {
+    const abilities = props.abilities
+    const keys = Object.keys(abilities);
+    const values = Object.values(abilities);
+
+    let items = [];
+    for (let i = 0; i < keys.length; i++) {
+        items.push(`+${values[i]} ${keys[i]}`);
+    }
+    console.log(items);
+
+    return (
+        <ul className="card__abilities">
+            {items.map(item => <li key={item}>{item}</li>)}
+        </ul>
+    ); 
+    
+}
+
 export const Card = (props) => {
     let cardType = props.card.class;
-    console.log(cardType);
     cardType = cardType.join('-');
 
     const icon = (box) => {
         switch (box) {
             case 'Dominion2':
-                return <img src="/img/dominion2-icon.svg" alt="Dominion Icon" class="card__icon_img" />
+                return <img src="/img/dominion2-icon.svg" alt="Dominion Icon" className="card__icon_img" />
             case 'Intrigue2':
-                return <img src="/img/intrigue2-icon.svg" alt="Intrigue Icon" class="card__icon_img" />
+                return <img src="/img/intrigue2-icon.svg" alt="Intrigue Icon" className="card__icon_img" />
             case 'Adventures':
-                return <img src="/img/adventures-icon.svg" alt="Adventures Icon" class="card__icon_img" />
+                return <img src="/img/adventures-icon.svg" alt="Adventures Icon" className="card__icon_img" />
             case 'Nocturne':
-                return <img src="/img/nocturne-icon.svg" alt="Nocturne Icon" class="card__icon_img" />
+                return <img src="/img/nocturne-icon.svg" alt="Nocturne Icon" className="card__icon_img" />
             default:
                 console.log('Error rendering icon');
         }
     }
+
+
 
     let cardClass = (type) => {
         let name = 'card';
@@ -36,6 +56,7 @@ export const Card = (props) => {
     }
 
     const heirloom = () => props.card.heirloom ? <span className="card__heirloom">Heirloom: {props.card.heirloom}</span> : null;
+    const abilities = (abilities) => abilities ? <AbilityList abilities={props.card.abilities} /> : null;
 
     return (
         <section className={cardClass(props.card.class)}>
@@ -43,6 +64,7 @@ export const Card = (props) => {
             <div className="card__icon_container">{icon(props.card.box)}</div>
             <div className="card__divider" />
             <div className="card__text_container">
+                {abilities(props.card.abilities)}
                 <p className="card__text">{props.card.text}</p>
             </div>
             <div className="card__bottom_banner">
