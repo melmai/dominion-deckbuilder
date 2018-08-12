2c7812a2d7293e21ecfc250d49fe707741637bae
Melissa Wong <mellymai@gmail.com>
2018-08-12 16:33:01 -0700

    Adds Separator to Nav

--- src/components/basic/Nav.jsx
+++ src/components/basic/Nav.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Nav = () => (
-    <nav className="nav">
-        <NavLink to="/" exact activeClassName="nav__item-active">DeckBuilder</NavLink>
-        <NavLink to="/cards" activeClassName="nav__item-active">Card Library</NavLink>
-        <NavLink to="/decks" activeClassName="nav__item-active">Suggested Decks</NavLink>
-    </nav>
-);
+const Nav = () => {
+    const Separator = () => <span> | </span>
+    return (    
+        <nav className="nav">
+            <NavLink to="/" exact activeClassName="nav__item-active">DeckBuilder</NavLink>
+            <Separator />
+            <NavLink to="/cards" activeClassName="nav__item-active">Card Library</NavLink>
+            <Separator />
+            <NavLink to="/decks" activeClassName="nav__item-active">Suggested Decks</NavLink>
+        </nav>
+    );
+
+};
 
 export default Nav;
