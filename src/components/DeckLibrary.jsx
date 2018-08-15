0b875f2f4820727d0be85820876c19c7eb85ddbe
Melissa Wong <mellymai@gmail.com>
2018-08-14 18:21:03 -0700

    DeckRows show name and expansions

--- src/components/DeckLibrary.jsx
+++ src/components/DeckLibrary.jsx
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
 //import Deck from './Deck';
 
-const DeckRow = props => <tr className="table__row"><td>{props.deck.name}</td></tr>;
+const DeckRow = props => {
+    const expansions = [];
+    let { adventures, dominion, intrigue, nocturne } = props.deck.cards;
+    if (adventures.length > 0) expansions.push('Adventures');
+    if (dominion.length > 0) expansions.push('Dominion');
+    if (intrigue.length > 0) expansions.push('Intrigue');
+    if (nocturne.length > 0) expansions.push('Nocturne');
+
+    return (
+        <tr className="table__row">
+            <td>{props.deck.name}</td>
+            <td>{expansions}</td>
+        </tr>
+    )
+}
 
 const DeckTable = props => {
     const decks = props.decks;
@@ -12,6 +26,7 @@ const DeckTable = props => {
             <thead className="table__header">
                 <tr>
                     <td>Name</td>
+                    <td>Expansions</td>
                 </tr>
             </thead>
             <tbody>
