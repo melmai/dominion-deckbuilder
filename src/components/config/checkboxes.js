9b5eecad9f4d65ddcdf1a517e5003dc55ce50c86
Melissa Wong <mellymai@gmail.com>
2018-09-05 14:26:05 -0700

    Adds options for Intrigue and Dom/Int

--- src/components/config/checkboxes.js
+++ src/components/config/checkboxes.js
@@ -66,3 +66,155 @@ export const dominion = [
     },
 ];
 
+export const intrigue = [ // add treasure, remove immunity
+    {
+        name: 'actions',
+        key: 'actions',
+        label: 'At least 1 card with +2 Actions'
+    },
+    {
+        name: 'buys',
+        key: 'buys',
+        label: 'At least 1 card with +1 Buys'
+    },
+    {
+        name: 'cards',
+        key: 'cards',
+        label: 'At least 1 card that allows drawing >2 cards'
+    },
+    {
+        name: 'victory',
+        key: 'victory',
+        label: 'At least 1 Victory Card'
+    },
+    {
+        name: 'no-victory',
+        key: 'no-victory',
+        label: 'No Victory Cards'
+    },
+    {
+        name: 'treasure',
+        key: 'treasure',
+        label: 'At least 1 Treasure Card'
+    },
+    {
+        name: 'no-treasure',
+        key: 'no-treasure',
+        label: 'No Treasure Cards'
+    },
+    {
+        name: 'attack',
+        key: 'attack',
+        label: 'At least 1 Attack Card'
+    },
+    {
+        name: 'no-attack',
+        key: 'no-attack',
+        label: 'No Attack Cards'
+    },
+    {
+        name: 'curses',
+        key: 'curses',
+        label: 'At least 1 card that gives Curses',
+    },
+    {
+        name: 'no-curses',
+        key: 'no-curses',
+        label: 'No Curses',
+    },
+    {
+        name: 'attack-reaction',
+        key: 'attack-reaction',
+        label: 'If Attack Card, include Reaction',
+    },
+    {
+        name: 'no-attack-reaction',
+        key: 'no-attack-reaction',
+        label: 'If no Attack Card, exclude Reactions',
+    },
+    {
+        name: 'trash',
+        key: 'trash',
+        label: 'At least 1 card with trash ability',
+    },
+];
+
+export const dominion_intrigue = [ // add treasure, add immunity
+    {
+        name: 'actions',
+        key: 'actions',
+        label: 'At least 1 card with +2 Actions'
+    },
+    {
+        name: 'buys',
+        key: 'buys',
+        label: 'At least 1 card with +1 Buys'
+    },
+    {
+        name: 'cards',
+        key: 'cards',
+        label: 'At least 1 card that allows drawing >2 cards'
+    },
+    {
+        name: 'victory',
+        key: 'victory',
+        label: 'At least 1 Victory Card'
+    },
+    {
+        name: 'no-victory',
+        key: 'no-victory',
+        label: 'No Victory Cards'
+    },
+    {
+        name: 'treasure',
+        key: 'treasure',
+        label: 'At least 1 Treasure Card'
+    },
+    {
+        name: 'no-treasure',
+        key: 'no-treasure',
+        label: 'No Treasure Cards'
+    },
+    {
+        name: 'attack',
+        key: 'attack',
+        label: 'At least 1 Attack Card'
+    },
+    {
+        name: 'no-attack',
+        key: 'no-attack',
+        label: 'No Attack Cards'
+    },
+    {
+        name: 'curses',
+        key: 'curses',
+        label: 'At least 1 card that gives Curses',
+    },
+    {
+        name: 'no-curses',
+        key: 'no-curses',
+        label: 'No Curses',
+    },
+    {
+        name: 'attack-immunity',
+        key: 'attack-immunity',
+        label: 'If Attack Card, include Immunity',
+    },
+    {
+        name: 'attack-reaction',
+        key: 'attack-reaction',
+        label: 'If Attack Card, include Reaction',
+    },
+    {
+        name: 'no-attack-reaction',
+        key: 'no-attack-reaction',
+        label: 'If no Attack Card, exclude Reactions',
+    },
+    {
+        name: 'trash',
+        key: 'trash',
+        label: 'At least 1 card with trash ability',
+    },
+];
+
+
