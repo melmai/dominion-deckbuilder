e3b44feb798e0b2be42c27808b22a3e0ca7fbc2f
Melissa Wong <mellymai@gmail.com>
2018-09-18 20:12:37 -0700

    Rearrange file structure

--- src/components/Result.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Deck } from './Deck';
-
-const Result = (props) => {
-    const deck = props.cards.length > 0 ? <Deck cards={props.cards} /> : 'No cards';
-    return deck;
-}
-
-export default Result;
--- src/components/basic/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Button = (props) => {
-    return(
-        <button className={props.className} type="button" onClick={(e) => props.onClick(e)}>{props.children}</button>
-    );
-}
-
-export default Button;
